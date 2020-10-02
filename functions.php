ee391226ff2ab7a613715ce32f0c7bc97a8488a2
idutsu <idutsudaisuke@gmail.com>
2020-10-02 16:52:16 +0900

    md

--- functions.php
+++ functions.php
@@ -2,5 +2,5 @@
 
 add_action( 'wp_enqueue_scripts', function(){
     wp_enqueue_script( 'form-js',THEME_URL.'/libs/mmm-form/form.js', array('contact-form-7'), filemtime( dirname(__FILE__).'/form.js'), true );
-    wp_enqueue_style( 'form-style', THEME_URL.'/libs/mmm-form/style.css',array('single-style'), filemtime( dirname(__FILE__).'/style.css') );
+    wp_enqueue_style( 'form-style', THEME_URL.'/libs/mmm-form/style.css',array('theme-style'), filemtime( dirname(__FILE__).'/style.css') );
 });
